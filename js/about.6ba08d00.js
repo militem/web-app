df834fb537165a1e48f59132cdb80c558a081ea6
militem <terrerosalex6382@gmail.com>
2023-02-13 07:52:30 -0500

    Add files via upload

---
+++ js/about.6ba08d00.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkproyecto_amv"]=self["webpackChunkproyecto_amv"]||[]).push([[443],{5399:function(t,n,u){u.r(n),u.d(n,{default:function(){return l}});var e=function(){var t=this;t._self._c;return t._m(0)},s=[function(){var t=this,n=t._self._c;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],a=u(1001),r={},c=(0,a.Z)(r,e,s,!1,null,null,null),l=c.exports}}]);
+//# sourceMappingURL=about.6ba08d00.js.map
